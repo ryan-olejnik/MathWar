0befeb235068f4dd0a7f027d069bf6ae45f8c7c5
Ryan <ryan.olejnik@gmail.com>
2018-05-21 23:38:17 +0000

    generate_question now modifies the players score when they answer incorrctly

--- game.rb
+++ game.rb
@@ -4,43 +4,51 @@ class Game
   def initialize(player1Name, player2Name)
     @player1 = Player.new(player1Name)
     @player2 = Player.new(player2Name)
+    @current_players_turn = 1
   end
 
-  # def generate_question(player)
-  #   while true
-  #     num1 = rand(9)+1
-  #     num2 = rand(9)+1
-  #     operator_id = rand(3)+1
+  def generate_question(player)
+    num1 = rand(9)+1
+    num2 = rand(9)+1
+    operator_id = rand(3)+1
 
 
-  #     case operator_id
-  #     when 1
-  #       operator = 'plus'
-  #       answer = num1 + num2
-  #     when 2
-  #       operator = 'minus'
-  #       answer = num1 - num2
-  #     when 3
-  #       operator = 'multiplied by'
-  #       answer = num1 * num2
-  #     end
+    case operator_id
+    when 1
+      operator = 'plus'
+      answer = num1 + num2
+    when 2
+      operator = 'minus'
+      answer = num1 - num2
+    when 3
+      operator = 'multiplied by'
+      answer = num1 * num2
+    end
 
 
-  #     print "#{player}, What is #{num1} #{operator} #{num2}?  >"
+    print "#{player.name}, What is #{num1} #{operator} #{num2}?  >"
 
-  #     player_answer = gets.chomp()
+    player_answer = gets.chomp()
 
-  #     if player_answer.to_i == answer.to_i then
-  #       puts "You are correct!!!"
-  #     else
-  #       puts "WRONGGGGGG!!!!!"
-  #     end
-  #   end
-  # end
+    if player_answer.to_i == answer.to_i then
+      puts "#{player.name}... You are correct!!!"
+    else
+      puts "#{player.name}... YOU ARE WRONGGGGGG!!!!!"
+      player.score -= 1
+    end
+  end
 
 
   def run
+    
     puts "Welcome #{@player1.name} and #{@player2.name} !"
+
+    puts "scores are: #{@player1.name}: #{@player1.score}, #{@player2.name}: #{@player2.score}"
+
+    generate_question(@player1)
+    puts "scores are: #{@player1.name}: #{@player1.score}, #{@player2.name}: #{@player2.score}"
+
+
   end
 
 end
--- player.rb
+++ player.rb
@@ -1,5 +1,6 @@
 class Player
   attr_reader :name
+  attr_accessor :score
   def initialize(name)
     @name = name
     @score = 3
